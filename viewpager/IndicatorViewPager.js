9a72969d97fd3d2ae2d2e8802578ac201db81c26
Sergio <smaisidoro@gmail.com>
2023-09-11 23:06:21 +0300

    Deprecate ViewPropTypes

--- viewpager/IndicatorViewPager.js
+++ viewpager/IndicatorViewPager.js
@@ -6,7 +6,9 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, View, ViewPropTypes } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import {ViewPropTypes} from 'deprecated-react-native-prop-types'
+
 import ViewPager from './ViewPager'
 
 export default class IndicatorViewPager extends Component {
